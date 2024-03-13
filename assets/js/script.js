696b5e6c4c6deb2d6a685be0861b3206f442e846
Tom Widdows <thomas.widdows10@gmail.com>
2024-03-13 17:07:02 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,12 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 let gameType = this.getAttribute("data-type")
                 alert(`you clicked ${gameType}`);
+                runGame(gameType)
             }
         })
     }
+
+    runGame("addition");
 }
 )
 
@@ -21,11 +24,18 @@ document.addEventListener("DOMContentLoaded", function() {
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
 
     //Creates two random numbers bewtween 1 & 25
     let num1 = Math.ceil(Math.random() * 25);
-    let num2 = Math.ceil(Math.random() * 25);
+    let num2 = Math.ceil(Math.random() * 25);#
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`
+    }
 
 }
 
@@ -45,8 +55,10 @@ function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+"
 }
 
 function displaySubtractQuestion() {
